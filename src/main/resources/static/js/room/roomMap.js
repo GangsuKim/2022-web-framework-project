38e449736fe6fa2c232100f79810e6c1f81aeede
GangsuKim <gangsu1813@gmail.com>
2022-12-16 08:25:42 +0900

    CP map -1

--- src/main/resources/static/js/room/roomMap.js
+++ src/main/resources/static/js/room/roomMap.js
@@ -1,8 +1,11 @@
 var geocoder = new kakao.maps.services.Geocoder(); // GeoCoder 객체
 var ps = new kakao.maps.services.Places();
 var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+var posx, posy;
 geocoder.addressSearch(getParameter("id"), function (res, status) {
     if (status === kakao.maps.services.Status.OK) {
+        posx = res[0].x;
+        posy = res[0].y;
         var coords = new kakao.maps.LatLng(res[0].y, res[0].x);
         var marker = new kakao.maps.Marker({
             map: map,
@@ -12,7 +15,7 @@ geocoder.addressSearch(getParameter("id"), function (res, status) {
 });
 var options = {
     //지도를 생성할 때 필요한 기본 옵션
-    center: new kakao.maps.LatLng(37.88355, 127.738202),
+    center: new kakao.maps.LatLng(posy, posx),
     level: 3, //지도의 레벨(확대, 축소 정도)
 };
 var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
--- src/main/resources/static/ts/room/roomMap.ts
+++ src/main/resources/static/ts/room/roomMap.ts
@@ -1,9 +1,13 @@
 var geocoder = new kakao.maps.services.Geocoder(); // GeoCoder 객체
 var ps = new kakao.maps.services.Places();
 var container = document.getElementById("map"); //지도를 담을 영역의 DOM 레퍼런스
+let posx: Number, posy: Number;
 
 geocoder.addressSearch(getParameter("id"), (res, status) => {
     if(status === kakao.maps.services.Status.OK) {
+        posx = res[0].x;
+        posy = res[0].y;
+
         var coords = new kakao.maps.LatLng(res[0].y, res[0].x);
 
         var marker = new kakao.maps.Marker({
@@ -16,7 +20,7 @@ geocoder.addressSearch(getParameter("id"), (res, status) => {
 
 var options = {
     //지도를 생성할 때 필요한 기본 옵션
-    center: new kakao.maps.LatLng(37.88355, 127.738202), //지도의 중심좌표.
+    center: new kakao.maps.LatLng(posy, posx), //지도의 중심좌표.
     level: 3, //지도의 레벨(확대, 축소 정도)
 };
 
